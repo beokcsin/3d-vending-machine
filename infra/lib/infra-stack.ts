ecb2d6150beb238a1421f9adc5dd95830b41016d
Horváth Gergő Zsombor <horvgergi@gmail.com>
2025-07-28 20:26:59 +0200

    feat: add cdk for infrastructure

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iot from 'aws-cdk-lib/aws-iot';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Print job bucket
+    const bucket = new s3.Bucket(this, 'PrintJobBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // IoT Policy for devices
+    new iot.CfnPolicy(this, 'EdgeDevicePolicy', {
+      policyName: 'EdgeDevicePolicy',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Effect: 'Allow',
+            Action: ['iot:Connect'],
+            Resource: '*',
+          },
+          {
+            Effect: 'Allow',
+            Action: ['iot:Publish', 'iot:Subscribe', 'iot:Receive'],
+            Resource: [
+              `arn:aws:iot:${this.region}:${this.account}:topic/test/3dprinter/*`
+            ],
+          }
+        ],
+      },
+    });
   }
 }
